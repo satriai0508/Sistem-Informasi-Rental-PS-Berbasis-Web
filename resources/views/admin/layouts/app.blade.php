1ee60d0448c9bdf91fbd7ec17a42fd1d6a40c23d
satriai0508 <satriai418@gmail.com>
2022-11-13 22:53:37 +0800

    section to index

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -166,11 +166,7 @@
         <!-- ============================================================== -->
         <div class="row">
           <div class="col-12">
-            <div class="card">
-              <div class="card-body">
-                This is some text within a card block.
-              </div>
-            </div>
+            @yield('content')
           </div>
         </div>
         <!-- ============================================================== -->
