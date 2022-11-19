9d9b5e00f5dc69fbab8455b7392fc75d04036e0e
satriai0508 <satriai418@gmail.com>
2022-11-19 12:03:48 +0800

    fix layouting in admin

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,7 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\RentController;
+use App\Http\Controllers\{HomeController, DeviceController, RentController};
 
 /*
 |--------------------------------------------------------------------------
@@ -26,6 +25,6 @@ Auth::routes();
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 Route::middleware(['auth'])->group(function () {
-    Route::resource('device', DeviceController::class)->except(['show']);
-    Route::resource('rent', RentController::class)->except(['show']);
+    Route::resource('devices', DeviceController::class)->except(['show']);
+    Route::resource('rents', RentController::class)->except(['show']);
 });
